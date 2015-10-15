4a71a6c0e5859646364e65fef6f4f5ff14535b36
Alex Gaskill <ajgaskill08@gmail.com>
2015-10-14 19:48:57 -0700

    started working on the GUI but there are still some GUI breaking bugs that i need to figure out why they are there next time i work on it.

--- src/snrt/SNRT.java
+++ src/snrt/SNRT.java
@@ -4,9 +4,11 @@
  * and open the template in the editor.
  */
 package snrt;
+import java.awt.GridLayout;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import javax.swing;
+import javax.swing.*;
 
 /**
  *
@@ -17,23 +19,40 @@ public class SNRT {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //code from ramayac on stackover flow... begin imported code...
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame();
+        
+        // Adding the dementions and title of the window used for the reporting
+        // Tool
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Simple Network Reporting Tool ");
+        frame.setLocation(500,400);
+        frame.setSize(500,500);
+        
+        JPanel panel = new JPanel(new GridLayout(1,10){
+            //code from ramayac on stackover flow... begin imported code...
+            
+            String line;
             try {
-        String line;
-         Process p = Runtime.getRuntime().exec
-        (System.getenv("windir") +"\\system32\\"+"tasklist.exe");
-        BufferedReader input =
-                new BufferedReader(new InputStreamReader(p.getInputStream()));
-        while ((line = input.readLine()) != null) {
-            System.out.println(line); //<-- Parse data here.
+            Process p = Runtime.getRuntime().exec
+                (System.getenv("windir") +"\\system32\\"+"tasklist.exe");
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                add(line, frame);
+            }
+            input.close();
         }
-        input.close();
-    } 
             catch (Exception err)
             {
                 err.printStackTrace();
             }
-        }
-    // end of imported code...
+        };
+                // end of imported code...
+                
+        });
+ 
+
+    public SNRT() {
+    }
  }
