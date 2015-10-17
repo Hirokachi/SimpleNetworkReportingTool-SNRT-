3c74d7538e8f7a0bf4fb009bd1084d4808402cf0
Alex Gaskill <ajgaskill08@gmail.com>
2015-10-17 16:50:41 -0700

    Tried making the window display the out put of the getPID class, never got it to work the way i expect.
    Commit Anyway.

--- src/snrt/GUIClass.java
+++ src/snrt/GUIClass.java
@@ -21,11 +21,14 @@ public class GUIClass extends JPanel
                 implements ActionListener{
     
     protected JButton getProcess, nextComputer; 
-   
+    protected static JLabel processes;
+    protected static JPanel panelForText;
+    
     /**
     * Needs nothing
     */
     public GUIClass() {
+        
         
         getProcess = new JButton("Go Get Processes!");
         getProcess.setVerticalTextPosition(AbstractButton.CENTER);
@@ -44,10 +47,10 @@ public class GUIClass extends JPanel
     public void actionPerformed(ActionEvent e) {
         
         getPID pID = new getPID();
-        JLabel processes = new JLabel("Test");
-                
+        
         if ("goGetIt".equals(e.getActionCommand())){
             processes.setText(pID.getProcesses());
+            System.out.print(pID.getProcesses());
         }
         else {
             processes.setText("Could not find any processes!");
@@ -63,7 +66,17 @@ public class GUIClass extends JPanel
 
         //Create and set up the window.
         JFrame frame = new JFrame("Processes");
+        processes = new JLabel("Test");
+        panelForText = new JPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        /*
+        *  Add the text field to the panel that will house the text field then
+        *  to the window.
+        */
+        panelForText.add(processes);
+        frame.add(panelForText);
+        
  
         //Create and set up the content pane.
         JComponent newContentPane = new GUIClass();
--- src/snrt/SNRT.java
+++ src/snrt/SNRT.java
@@ -19,9 +19,5 @@ public class SNRT {
         // Creates a new instance of the GUIClass for later use.
         GUIClass display = new GUIClass();
         display.createAndShowGUI();
-        // Creates a new instance of the getPID class for later use.
-        getPID pID = new getPID();
-        // Prints the processes out to the command line after running the method
-        System.out.println(pID.getProcesses());
     }
 }
