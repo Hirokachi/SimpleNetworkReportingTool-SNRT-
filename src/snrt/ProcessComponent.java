00cda07a6cad461dab8dfea1f6f713a0544ed12f
Alex Gaskill <ajgaskill08@gmail.com>
2015-10-22 09:39:54 -0700

    Build out KillSelectedProcess method
    Added some code to find the process ID  from the array to run the kill
    process command.
    Added cases to protect the OS from having a fatal error.

--- src/snrt/ProcessComponent.java
+++ src/snrt/ProcessComponent.java
@@ -7,21 +7,23 @@ package snrt;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.StringBuilder;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Alex Gaskill
  */
 public class ProcessComponent {
-        /**
-         * This method sets processInfo and returns that information to the 
-         * method/class that called it.
-         * @return: Will either return the process information, or "error: no 
-         * processes" if it couldn't find any processes running.
-         */
-        public String getProcesses(){
+    
+    /**
+     * This method sets processInfo and returns that information to the 
+     * method/class that called it.
+     * @return: Will either return the process information, or "error: no 
+     * processes" if it couldn't find any processes running.
+     */
+    public String getProcesses(){
             
-          try {
+        try {
                 StringBuilder controller = new StringBuilder("");
                 String line;
                 String processInfo;
@@ -44,23 +46,50 @@ public class ProcessComponent {
             }
         }
         
-        /**
-         * This method will end the task of a selected process on the GUI-
-         * Component side. 
-         * 
-         * @param process: the process id that has been selected through the
-         * GUIComponent and needs to be killed/ended.
-         */
-        public void killSelectedProcess (int process){
-            try{
+    /**
+     * This method will end the task of a selected process on the GUI-
+     * Component side. A little more work should be done here to get the PID
+     * from the string of the process selected.
+     * 
+     * @param processInfo: the process string that has been selected through 
+     * the GUIComponent and needs to be killed/ended.
+     */
+    public void killSelectedProcess (String[] processInfo) {
+
+
+        String processID = "NAN";
+
+        //find the PID from the processInfo String.
+        for (String correctPID : processInfo) {
+            if(correctPID.equals("[0-9]+"))
+                processID = correctPID;
+        }
+        
+        //Created case to not cause a fatal error in the OS with no Process ID.
+        //Otherwise, go back to trying to end the task of selected Process ID. 
+        if(processID.equals("NAN")) {
+               JOptionPane.showMessageDialog(null, "No Process ID was "
+                       + "found; was no process selected?", "EndTask Error:",
+                       JOptionPane.OK_OPTION);
+        }
+        else {
+            try {
+                // Create a runtime object so that when we try to kill a process
+                // on the computer we are selecting it will be more successful
+                // causing the computer to kill the process by verifying the
+                // OS and running the command line to kill it. 
                 Runtime rt = Runtime.getRuntime();
                 if (System.getProperty("os.name").contains("windows")) 
-                    rt.exec("taskkill " + process);
+                    rt.exec("taskkill " + processID);
                 else
-                    rt.exec("kill -9 " + process);
+                    rt.exec("kill -9 " + processID);
             }
-            catch (Exception err){
+            catch (Exception err) {
                 err.printStackTrace();
+                JOptionPane.showMessageDialog(null, "No Process ID was "
+                       + "found or Process was already ended" , "EndTask Error:"
+                        , JOptionPane.OK_OPTION);
             }
         }
+    }
 }
