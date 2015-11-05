3251a7a195160694177df7b0bce82fc1d4aba58d
Alex Gaskill <ajgaskill08@gmail.com>
2015-11-04 21:46:23 -0800

    Add method nextComputer
    gets the computers name and IP address for the computer that runs the
    program.

--- src/snrt/ProcessComponent.java
+++ src/snrt/ProcessComponent.java
@@ -7,6 +7,9 @@ package snrt;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Vector;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -52,9 +55,7 @@ public class ProcessComponent {
         }
         
     /**
-     * This method will end the task of a selected process on the GUI-
-     * Component side. A little more work should be done here to get the PID
-     * from the string of the process selected.
+     * This method will end the task of a selected process.
      * 
      * @param processID: the process string that has been selected through 
      * the GUIComponent and needs to be killed/ended.
@@ -75,5 +76,38 @@ public class ProcessComponent {
         catch (Exception err) {
             err.printStackTrace();
         }
+    }
+    /**
+     * 
+     * @return 
+     */
+    public Vector nextComputer() {
+        
+        String hostName;
+        String hostIP;
+        Vector hostInfo = new Vector();
+        Vector hostError = new Vector();
+        hostError.add("Error");
+        
+        try {
+            //find the host name and ip v4 address and throw that into a string
+            InetAddress addr;
+            addr = InetAddress.getLocalHost();
+            hostName = addr.getHostName();
+            hostIP = Inet4Address.getLocalHost().toString();
+            
+            //add the hostIP and hostIP that was aquired into a vector.
+            hostInfo.add(hostIP);
+            hostInfo.add(hostName);
+            
+            return (hostInfo);
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            
+            //return an one line vector that says error.
+            return (hostError);
+        }
+        
     }
 }
